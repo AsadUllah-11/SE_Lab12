ee59b968cfbabb8508df5e324964575fc9f5079e
AsadUllah-11 <2022cs11@student.uet.edu.pk>
2024-11-25 09:43:56 +0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { getPostData } from '../lib/posts';
+
+export default function Home() {
+  const { data, content } = getPostData();
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  );
+}
